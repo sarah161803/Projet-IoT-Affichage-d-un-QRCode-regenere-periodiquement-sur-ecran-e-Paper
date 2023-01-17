a88d73e3a1074bc35f5f27653696e93bd36966d0
sarah161803 <84006697+sarah161803@users.noreply.github.com>
2023-01-17 10:56:41 +0100

    Add files via upload
    Ajout de la regénération du qr-code

--- idosens/epd.c
+++ idosens/epd.c
@@ -17,6 +17,31 @@
  *
  * @}
  */
+//Include time
+#include <time.h>
+#include <string.h>
+#define DEFAULT_TM {0,0,0,1,0,121,0,0,0}
+
+#include <inttypes.h>
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#include "hashes/aes128_cmac.h"
+#include "crypto/ciphers.h"
+
+
+// 1972 and 1976 have 366 days (DELTA_EPOCH_GPS is 315964800 seconds)
+// GPS Epoch consists of a count of weeks and seconds of the week since 0 hours (midnight) Sunday 6 January 1980
+#define DELTA_EPOCH_GPS ((365*8 + 366*2 + 5)*(24*60*60))
+
+// The end-device responds by sending up to NbTransmissions AppTimeReq messages
+// with the AnsRequired bit set to 0.
+// The end-device stops re-transmissions of the AppTimeReq if a valid AppTimeAns is received.
+// If the NbTransmissions field is 0, the command SHALL be silently discarded.
+// The delay between consecutive transmissions of the AppTimeReq is application specific.
+// TODO static unsigned int NbTransmissions = 0;
+
 
 ///////Include  Epaper
 #include "epd_board.h"
@@ -36,12 +61,7 @@
 #include "qrcodegen.h"
 #include "disp_dev.h"
 
-//AJOUT DES DEFINE DE RIOT
-#define MESSAGE_TO_ENCODE   "https://chamilo.univ-grenoble-alpes.fr/"
-#define ENCODER_VERSION     (sizeof(MESSAGE_TO_ENCODE) >> 3)
-static uint8_t qr0[qrcodegen_BUFFER_LEN_FOR_VERSION(ENCODER_VERSION)];
-static uint8_t buffer[qrcodegen_BUFFER_LEN_FOR_VERSION(ENCODER_VERSION)];
-
+//Definition size e paper screen
 #define IMAGE_W (264)
 #define IMAGE_H (176)
 #define IMAGE_LEN (IMAGE_W*IMAGE_H/8)
@@ -57,6 +77,20 @@ static void memcopy(const uint8_t * buffsource, uint8_t * buffdest, uint16_t len
     {
         buffdest[i]=buffsource[i];
     }
+}
+
+//Give number epoch
+unsigned int getTimeSinceEpoch(void) {
+	struct tm current_time = DEFAULT_TM;
+	// Read the RTC current time
+#if MODULE_PERIPH_RTC == 1
+	rtc_get_time(&current_time);
+#endif
+	//print_time("[clock] Current time: ", &current_time);
+	time_t timeSinceEpoch = mktime(&current_time);
+	// substract number of seconds between 6/1/1980 and 1/1/1970
+	timeSinceEpoch -= DELTA_EPOCH_GPS;
+	return timeSinceEpoch;
 }
 
 /**
@@ -77,47 +111,88 @@ void epd_init(void)
 /**
  * Draw default picture on EPD
  */
+ 
+ 
 void epd_draw_default(void)
 {
-    puts("Default");
-    EPD_power_on(EPD_270, 18);
-    puts("drive on");
-    EPD_initialize_driver ();
-    puts("drive init");
-//    memcopy(default_icon,image,IMAGE_LEN);
-//    memcopy(epd_bitmap_qrcode,image,IMAGE_LEN);
+	puts("Default");
+	EPD_power_on(EPD_270, 18);
+	puts("drive on");
+	EPD_initialize_driver ();
+	puts("drive init");
 
-//    BD_fillrect(0, 0, 50, 50, 0);
+	//URL SANS CRYPTO
+	char URL_TO_ENCODE[50];
+	sprintf(URL_TO_ENCODE, "https://presence.com/%x", getTimeSinceEpoch());
+	printf("%s", URL_TO_ENCODE);
 
-    qrcodegen_encodeText(MESSAGE_TO_ENCODE,
+
+
+	//URL AVEC CRYPTO
+	char URL_TO_ENCODE_KEY[50];
+	
+	// AES128_CMAC_KEY and AES128_CMAC_KEY_VERSION can be set using downlink
+	uint16_t AES128_CMAC_KEY_VERSION=1;
+	const uint8_t AES128_CMAC_KEY[16] = {
+	    0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88,
+	    0x99, 0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66
+	};
+
+	// get the secure digest
+	char A_ENCODER[50];
+	sprintf(A_ENCODER, "%x/%x", getTimeSinceEpoch(), AES128_CMAC_KEY_VERSION);
+	uint8_t digest[16];
+	aes128_cmac_context_t ctx;
+	aes128_cmac_init(&ctx, AES128_CMAC_KEY, 16);
+	aes128_cmac_update(&ctx, A_ENCODER, 50);
+	aes128_cmac_final(&ctx, digest);
+
+
+	sprintf(URL_TO_ENCODE_KEY, "https://presence.com/%x/%x/%s", getTimeSinceEpoch(), AES128_CMAC_KEY_VERSION, digest);
+
+	
+	int ENCODER_VERSION = (sizeof(URL_TO_ENCODE) >> 3);
+	
+	
+	uint8_t qr0[qrcodegen_BUFFER_LEN_FOR_VERSION(ENCODER_VERSION)];
+	uint8_t buffer[qrcodegen_BUFFER_LEN_FOR_VERSION(ENCODER_VERSION)];
+
+	
+	//Generation of qr code
+    	qrcodegen_encodeText(URL_TO_ENCODE,
                               buffer, qr0, qrcodegen_Ecc_MEDIUM,
                               qrcodegen_VERSION_MIN, qrcodegen_VERSION_MAX,
                               qrcodegen_Mask_AUTO, true);
-    int size = qrcodegen_getSize(qr0);
-    
-    int square_size = IMAGE_H / size;		//Size min screen
-    int x0 = (IMAGE_W - size*square_size) / 2;
-    int y0 = (IMAGE_H - size*square_size) / 2;
-    
-    for (int j = 0; j < size; j++){
-    	x0 = (IMAGE_W - size*square_size) / 2;
-    	for (int i = 0; i < size; i++){
-    		if (qrcodegen_getModule(qr0, i, j)){
-    			BD_fillrect(x0, y0, x0+square_size, y0+square_size, 0);
-    		}
-    		
-    		x0 += square_size;
-    	}
-	y0 += square_size;
-    }
-    
-
-    EPD_display_from_array_prt ( old_image, image );
-    puts("disp");
-    memcopy(default_icon,old_image,IMAGE_LEN);
-    EPD_power_off();
-    spi_release(EPD_PARAM_SPI);
-    puts("drive off");
+    	
+    	//Get size of qr code
+	int size = qrcodegen_getSize(qr0);
+	int square_size = IMAGE_H / size;		//Size min screen
+	
+	//Start after 0 to center the qr code
+	int x0 = (IMAGE_W - size*square_size) / 2;
+	int y0 = (IMAGE_H - size*square_size) / 2;
+	
+	
+	//Drawing the image
+	for (int j = 0; j < size; j++){
+		x0 = (IMAGE_W - size*square_size) / 2;
+	    	for (int i = 0; i < size; i++){
+	    		if (qrcodegen_getModule(qr0, i, j)){
+	    			BD_fillrect(x0, y0, x0+square_size, y0+square_size, 0); //Add square for each black pixel of qr code
+	    		}
+	    		
+	    		x0 += square_size;
+	    	}
+		y0 += square_size;
+	}
+	    
+	//e paper procedure to show the picture created
+	EPD_display_from_array_prt ( old_image, image );
+	puts("disp");
+	memcopy(default_icon,old_image,IMAGE_LEN);
+	EPD_power_off();
+	spi_release(EPD_PARAM_SPI);
+	puts("drive off");
 
 }
 /**
--- idosens/main.c
+++ idosens/main.c
@@ -40,6 +40,57 @@
 #include "sx127x_params.h"
 #include "sx127x_netdev.h"
 
+
+//AJOUTS LORAWAN
+
+#include "periph_conf.h"
+#include "clk.h"
+#include "timex.h"
+#include "ztimer.h"
+#include "at30tse75x.h"
+
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include "msg.h"
+
+#include "cayenne_lpp.h"
+
+#include "net/loramac.h"
+#include "semtech_loramac.h"
+
+#include "sx127x.h"
+
+
+#ifndef TXPERIOD
+#define TXPERIOD                       (60U)
+#endif
+
+#ifndef JOIN_RETRY_PERIOD
+#define JOIN_RETRY_PERIOD            (60U)
+#endif
+
+
+#define SENDER_PRIO         (THREAD_PRIORITY_MAIN - 1)
+static kernel_pid_t sender_pid;
+static char sender_stack[THREAD_STACKSIZE_MAIN / 2];
+
+static semtech_loramac_t loramac;
+
+//static ztimer_t timer;
+
+//static at30tse75x_t temp_driver;
+
+static uint8_t deveui[LORAMAC_DEVEUI_LEN];
+static uint8_t appeui[LORAMAC_APPEUI_LEN];
+static uint8_t appkey[LORAMAC_APPKEY_LEN];
+
+static cayenne_lpp_t lpp;
+
+
+
+
 ///////Include  Epaper
 #include "epd_board.h"
 #include "epd.h"
@@ -244,6 +295,59 @@ static void cb_btn(void * arg){
     }
 }
 
+
+static void _send_message(void)
+{
+
+    // TODO exit at30tse75x from deepsleep
+    float temp = 0.0f;
+
+    printf("Temperature: %f\n", temp);
+    cayenne_lpp_reset(&lpp);
+    cayenne_lpp_add_temperature(&lpp, 3, temp);
+    /* Try to send the message */
+    uint8_t ret = semtech_loramac_send(&loramac,
+                                            (uint8_t *) &lpp.buffer, sizeof(lpp.cursor));
+    if (ret != SEMTECH_LORAMAC_TX_DONE)  {
+        puts("Fail to send message");
+    } else {
+        puts("Message sent");
+    }
+    
+    
+    // TODO add other sensors measurement to lpp
+
+    // TODO set at30tse75x inot deepsleep mode
+
+    return;
+}
+
+
+
+static void *sender(void *arg)
+{
+    (void)arg;
+
+    msg_t msg;
+    msg_t msg_queue[8];
+    msg_init_queue(msg_queue, 8);
+
+    while (1) {
+        msg_receive(&msg);
+
+        /* Trigger the message send */
+        _send_message();
+
+        /* Schedule the next wake-up alarm */
+        //_prepare_next_alarm();
+    }
+
+    /* this should never be reached */
+    return NULL;
+}
+
+
+
 int main(void)
 {
     //Set interupt for the button used for waking up the board 
@@ -257,14 +361,37 @@ int main(void)
     
     epd_init();
 
+    while(1){	//A modifier si on rajoute la connexion LORA
+    //Regeneration du code
+    unsigned int time_in_epoch = getTimeSinceEpoch() + 60;	//Toutes les minutes
+    
     set_default();
-
+    
+    while(getTimeSinceEpoch() < time_in_epoch){}	//On bloque tant que cela ne fait pas une minute
+  
+    }
     //Init RTC
     rtc_poweron();
 
     //Init crypto
     aes_init(&cipher, IDO_KEY, 16);
     
+//Code Lora 
+if(0){
+    
+    /* Convert identifiers and application key */
+    fmt_hex_bytes(deveui, CONFIG_LORAMAC_DEV_EUI_DEFAULT);
+    fmt_hex_bytes(appeui, CONFIG_LORAMAC_APP_EUI_DEFAULT);
+    fmt_hex_bytes(appkey, CONFIG_LORAMAC_APP_KEY_DEFAULT);
+
+    /* Initialize the radio driver */
+    sx127x_setup(&sx127x, &sx127x_params[0], 0);
+    loramac.netdev = &sx127x.netdev;
+    loramac.netdev->driver = &sx127x_driver;
+    
+    
+    
+
     //Init LORA
     sx127x.params = sx127x_params[0];
     netdev_t *netdev = (netdev_t *)&sx127x;
@@ -285,6 +412,50 @@ int main(void)
                         &lora_sf, sizeof(lora_sf));
     netdev->driver->set(netdev, NETOPT_CODING_RATE,
                         &lora_cr, sizeof(lora_cr));
+
+
+//AJOUT DU CODE LORAWAN NON FONCTIONNEL
+/* Initialize the loramac stack */
+    semtech_loramac_init(&loramac);
+    semtech_loramac_set_deveui(&loramac, deveui);
+    semtech_loramac_set_appeui(&loramac, appeui);
+    semtech_loramac_set_appkey(&loramac, appkey);
+
+    // TODO printf deveui appeui appkey
+
+    /* Use a fast datarate, e.g. BW125/SF7 in EU868 */
+    semtech_loramac_set_dr(&loramac, LORAMAC_DR_5 );
+
+    /* Start the Over-The-Air Activation (OTAA) procedure to retrieve the
+     * generated device address and to get the network and application session
+     * keys.
+     */
+    puts("Starting join procedure");
+    while (semtech_loramac_join(&loramac, LORAMAC_JOIN_OTAA) != SEMTECH_LORAMAC_JOIN_SUCCEEDED) {
+        puts("Join procedure failed");
+        ztimer_sleep(ZTIMER_USEC, JOIN_RETRY_PERIOD * US_PER_SEC);
+
+        // TODO decrement DR before retrying
+        // semtech_loramac_set_dr(&loramac, LORAMAC_DR_5 );
+    }
+    puts("Join procedure succeeded");
+
+    // TODO Set ADR
+    // TODO Set Confirmed/Unconfirmed
+
+        /* start the sender thread */
+    sender_pid = thread_create(sender_stack, sizeof(sender_stack),
+                               SENDER_PRIO, 0, sender, NULL, "sender");
+
+    /* trigger the first send */
+    msg_t msg;
+    msg_send(&msg, sender_pid);
+
+
+
+
+
+//Programme LORAWAN NON Fonctionnel
 
     //start in idle mode
     set_idle();
@@ -329,5 +500,8 @@ int main(void)
             puts("Unexpected msg type");
         }
     }
+    }
+    
+    
     return 0;
 }
